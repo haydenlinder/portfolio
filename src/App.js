3e3c1919634446486d738536957e7935721adc26
haydenlinder <haydenlinder@gmail.com>
2020-11-25 21:00:35 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { useRef } from 'react';
+
+const Box = props => {
+  const boxRef = useRef();
+  useFrame(state => {
+    boxRef.current.rotation.x += 0.01;
+    boxRef.current.rotation.y += 0.01;
+  });
+
+  return(
+    <mesh ref={boxRef}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color={'blue'} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <Canvas>
+        <Box />
+      </Canvas>
     </div>
   );
 }
