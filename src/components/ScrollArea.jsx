cced1fb3a0adf92659fae4f08ebd0d12086a20d6
haydenlinder <haydenlinder@gmail.com>
2020-11-29 13:09:57 -0800

    Adjust scrolling

--- src/components/ScrollArea.jsx
+++ src/components/ScrollArea.jsx
@@ -2,7 +2,7 @@ import state from '../state';
 
 const ScrollArea = ({ pages = 1 }) => {
     const handleScroll = (e) => {
-        state.top = e.target.scrollTop
+        state.top = e.target.scrollTop/8
     }
 
     return(
